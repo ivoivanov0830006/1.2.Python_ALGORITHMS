faf0017acf4741af8acac3a44a7f70a3ab8c0eeb
ivoivanov0830006 <108527062+ivoivanov0830006@users.noreply.github.com>
2022-09-06 22:36:01 +0300

    Create bellman-ford.py

---
+++ 3.Graphs/bellman-ford.py
@@ -0,0 +1,24 @@
+# Find the shortest path in a graph and print it as a sequence
+# from S vertex to D vertex. Implement the Bellman-Ford algorithm.
+# Input
+# The input comes from the console.
+#    On the first line, you will receive an integer – n – the number of nodes.
+#    On the next line, you will receive an integer – e – the number of edges.
+#    On the next e lines, you will receive an edge in the following format: "{from} {to} {weight}".
+#    On the last two lines, you will receive source and destination nodes.
+# Output
+#    Print "Negative Cycle Detected" if detect a negative cycle.
+#    Otherwise, print the shortest path separated by a space and the total distance.
+# Input                             Output
+# 6                                 1 2 4 3 6
+# 8                                 7
+# 1 2 8
+# 1 3 10
+# 2 4 1
+# 3 6 2
+# 4 3 -4
+# 4 6 -1
+# 6 5 -2
+# 5 3 1
+# 1
+# 6
