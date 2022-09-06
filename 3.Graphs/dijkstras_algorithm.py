1a40d3d01aed83d0073cc99e1c80604b31e1e062
ivoivanov0830006 <108527062+ivoivanov0830006@users.noreply.github.com>
2022-09-06 22:31:45 +0300

    Update dijkstras_algorithm.py

--- 3.Graphs/dijkstras_algorithm.py
+++ 3.Graphs/dijkstras_algorithm.py
@@ -1,3 +1,61 @@
+from queue import PriorityQueue
+from collections import deque
+
+
+class Edge:
+    def __init__(self, source, destination, weight):
+        self.source = source
+        self.destination = destination
+        self.weight = weight
+
+
+edges = int(input())
+graph = {}
+
+for _ in range(edges):
+    source, destination, weight = [int(x) for x in input().split(', ')]
+    if source not in graph:
+        graph[source] = []
+    if destination not in graph:
+        graph[destination] = []
+    graph[source].append(Edge(source, destination, weight))
+
+start = int(input())
+target = int(input())
+
+max_node = max(graph.keys())
+
+distance = [float("inf")] * (max_node + 1)
+parent = [None] * (max_node + 1)
+
+distance[start] = 0
+
+pq = PriorityQueue()
+pq.put((0, start))
+
+while not pq.empty():
+    min_distance, node = pq.get()
+    if node == target:
+        break
+    for edge in graph[node]:
+        new_distance = min_distance + edge.weight
+        if new_distance < distance[edge.destination]:
+            distance[edge.destination] = new_distance
+            parent[edge.destination] = node
+            pq.put((new_distance, edge.destination))
+
+if distance[target] == float("inf"):
+    print("There is not such path.")
+else:
+    print(distance[target])
+
+    path = deque()
+    node = target
+    while node is not None:
+        path.appendleft(node)
+        node = parent[node]
+    print(*path, sep=" ")
+
 # Finding the shortest path between two nodes in an unweighted graph is done
 # by applying simple BFS. When we’re working with weighted graphs though,
 # things get more complicated. Dijkstra’s algorithm is one of the most famous
@@ -17,8 +75,9 @@
 #   Print all nodes that form the shortest path.
 #   If the end node can’t be reached from the start node, then you have to print: "There is no such path."
 # Input                                     Output
+#  *  start, end, distance
 # 18                                        42
-# 0, 6, 10        *  start, end, distance   0 8 5 4 11 1 9
+# 0, 6, 10                                  0 8 5 4 11 1 9
 # 0, 8, 12
 # 6, 4, 17
 # 6, 5, 6
